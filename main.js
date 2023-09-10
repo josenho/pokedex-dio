96cae9a0727e717a211ea347d3cf9c6ecb25b269
Jose Mateus <112735456+josenho@users.noreply.github.com>
2023-09-10 11:58:15 -0300

    Atualizando o arquivo main.js

--- main.js
+++ main.js
@@ -2,17 +2,18 @@
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
-                    <span class="number">#001</span>
-                    <span class="name">${pokemon.name}</span>
-                    <div class="detail">
-                        <ol class="types">
-                            <li class="type">grass</li>
-                            <li class="type">poison</li>
-                        </ol>
-                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
-                        alt="${pokemon.name}">
-                    </div>
-                </li>
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+                    alt="${pokemon.name}">
+            </div>
+        </li>
     `
 }
 
@@ -20,12 +21,8 @@ const pokemonList = document.getElementsByClassName('pokemonList');
 
 
 pokeApi.getPokemons().then((pokemons) => {
-    const listItems = [];
 
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            listItems.push(convertPokemonToLi(pokemon));
-        }
-
-        console.log(listItems);
+    const newList = pokemons.map((pokemons) => {
+        pokemonList.innerHtml += pokemons.map(convertPokemonToLi).join('');
     })
+})
